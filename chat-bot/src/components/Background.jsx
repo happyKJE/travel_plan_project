b85d073f2d78c6e65d7ed9698c6d7c74f594650f
happyKJE <jungeunkim2007@gmail.com>
2025-03-27 12:16:21 +0900

    [2025.03.27] jungeun 1. css 수정 중

--- chat-bot/src/components/Background.jsx
+++ chat-bot/src/components/Background.jsx
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Background.css";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Background = () => {
   const [show, setShow] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
   const location = useLocation();
-  const isVisible = (location.pathname === "/login") ||(location.pathname === "/join") ||
-                            (location.pathname === "/mypage");
 
-  const handleScroll = () => {
-    const currentScrollY = window.scrollY;
-    
-    if (currentScrollY > lastScrollY && currentScrollY > 50 ) {
-      // 아래로 스크롤하면 숨김
-      setShow(false);
-    } else if (currentScrollY < lastScrollY && currentScrollY < 80 && !isVisible) {
-      // 위로 스크롤해도 300 이하로 올라오면 보임
-      setShow(true);
-    }
+  const isHiddenPage = ["/login", "/join", "/mypage"].includes(location.pathname);
 
-    setLastScrollY(currentScrollY);
-  };
-  
+  //경로 바뀌면 show 변경
   useEffect(() => {
+    setShow(!isHiddenPage);
+  }, [isHiddenPage]);
+
+  //스크롤 이벤트
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 80) {
+        setShow(false);
+      } else if (currentScrollY < lastScrollY && currentScrollY <= 90 && !isHiddenPage) {
+        setShow(true);
+      }
+
+      lastScrollY = currentScrollY;
+    };
+
     window.addEventListener("scroll", handleScroll);
-    
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [lastScrollY]);
-
+  }, [isHiddenPage]);
 
   return (
     <div
@@ -38,10 +42,9 @@ const Background = () => {
       style={{
         opacity: show ? 1 : 0,
         transition: "opacity 1.5s ease",
-        pointerEvents: show ? 'auto' : 'none', 
+        pointerEvents: show ? "auto" : "none",
       }}
-    >
-    </div>
+    />
   );
 };
 
